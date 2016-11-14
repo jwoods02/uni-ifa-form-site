8028cb75219fb0c27e5c1cc01f7c9027fdaa6a87
James Woods <woodsj13@cardiff.ac.uk>
2016-11-14 12:33:15 +0000

    Updated server.py to process Ajax logins

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
-import Flask
+import os
+from flask import Flask, redirect, request, render_template, jsonify
 
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # [username, password]
-client_logins = []['james', 'password1']['mert', 'password2']]
+valid_logins = [['james', 'password1'], ['mert', 'password2']]
 
 @app.route("/CheckLogin", methods=['POST'])
 def checkLogin():
     print("Processing data")
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        the_login = [username, password]
+
+        if the_login in valid_logins:
+            return 'login successful'
+        else:
+            return 'login failed'
