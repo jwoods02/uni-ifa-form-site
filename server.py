d62f55faf392daaca13426a15c5c4f7441f3105e
James Woods <james@jwoods.me>
2016-11-15 12:23:50 +0000

    Updated server.py so that IFA's and clients logins return different messages.

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # [username, password]
-valid_logins = [['james', 'password1'], ['mert', 'password2']]
+valid_logins = [['james', 'password1', 'client'], ['mert', 'password2', 'IFA']]
 
 @app.route("/CheckLogin", methods=['POST'])
 def checkLogin():
@@ -14,12 +14,15 @@ def checkLogin():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        the_login = [username, password]
+        the_login_client = [username, password, "client"]
+        the_login_IFA = [username, password, "IFA"]
 
-        if the_login in valid_logins:
-            return 'login successful'
+        if the_login_client in valid_logins:
+            return 'login successful. User is client'
+        elif the_login_IFA in valid_logins:
+            return 'login successful. User is IFA'
         else:
-            return 'login failed'
+            return 'Login failed'
 
 
 if __name__ == "__main__":
