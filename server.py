f60253a002a0258c0a57ee2169bca087d2fd5b30
Mert Gol <golma@cardiff.ac.uk>
2016-11-16 20:05:00 +0000

    made changes to server file and the two client add pages, the routes to add new data to the databse was corrected

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask import Flask, redirect, request, render_template, jsonify
 
 app = Flask(__name__)
 
+DATABASE = 'database.db'
+
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # [username, password]
@@ -25,17 +27,19 @@ def checkLogin():
             return 'Login failed'
 
 
-@app.route("/Client/addClient", methods = ['POST'])
-def customerAddDetails():
-    firstName = request.form.get('firstName', default="Error")#rem: args for get form for post
-    surname = request.form.get('surname', default="Error")
-    termLocation = request.form.get('termLocation', default="Error")
-    homeLocation = request.form.get('homeLocation', default="Error")
+@app.route("/Client/ClientInsert", methods = ['POST'])
+def ClientAddDetails():
+    AccountID = request.form.get('AccountID', default="Error")#rem: args for get form for post
+    Forname = request.form.get('Forname', default="Error")
+    Surname = request.form.get('Surname', default="Error")
+    eMail = request.form.get('eMail', default="Error")
+    Username = request.form.get('Username', default="Error")
+    Password = request.form.get('Password', default="Error")
     try:
         conn = sqlite3.connect(DATABASE)
         cur = conn.cursor()
-        cur.execute("INSERT INTO Customers ('firstName', 'surname', 'termLocation', 'homeLocation')\
-                     VALUES (?,?,?,?)",(firstName, surname, termLocation, homeLocation) )
+        cur.execute("INSERT INTO Clients ('AccountID', 'Forname', 'Surname', 'eMail', 'Username', 'Password')\
+                     VALUES (?,?,?,?)",(AccountID, Forname, Surname, eMail, Username, Password) )
         conn.commit()
         msg = "Record successfully added"
     except:
