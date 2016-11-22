c39cae9cd227c3ef1790a859778fdfc80ff670dc
Laura Vuilleumier <vuilleumierl@cardiff.ac.uk>
2016-11-22 11:53:15 +0000

    Cleaned up code for server.py

--- server.py
+++ server.py
@@ -8,12 +8,10 @@ DATABASE = 'database.db'
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-# [username, password]
-valid_logins = [['james', 'password1', 'client'], ['mert', 'password2', 'IFA']]
 
 @app.route("/Login")
 def login():
-    return render_template('login/login.html', msg = '')
+    return render_template('login/login.html', msg='')
 
 
 @app.route("/CheckLogin", methods=['POST'])
@@ -24,7 +22,8 @@ def checkLogin():
         password = request.form['password']
         conn = sqlite3.connect(DATABASE)
         cur = conn.cursor()
-        cur.execute("SELECT * FROM Accounts WHERE Username=? AND Password=?", (username, password))
+        cur.execute("SELECT * FROM Accounts WHERE Username=? AND Password=?",
+                    (username, password))
         outcome = cur.fetchall()
         if len(outcome) > 0:
             return "/Client"
@@ -32,9 +31,9 @@ def checkLogin():
             return "/Login"
 
 
-@app.route("/Client/ClientInsert", methods = ['POST'])
+@app.route("/Client/ClientInsert", methods=['POST'])
 def ClientAddDetails():
-    AccountID = request.form.get('AccountID', default="Error")  #rem: args for get form for post
+    AccountID = request.form.get('AccountID', default="Error")
     Forname = request.form.get('Forname', default="Error")
     Surname = request.form.get('Surname', default="Error")
     eMail = request.form.get('eMail', default="Error")
@@ -43,15 +42,16 @@ def ClientAddDetails():
 
     conn = sqlite3.connect(DATABASE)
     details = [(AccountID, Forname, Surname, eMail, Username, Password)]
-    conn.executemany("INSERT INTO `Accounts`('AccountID', 'Forname', 'Surname', 'eMail', 'Username', 'Password')\
-                    VALUES (?,?,?,?,?,?)",details)
+    conn.executemany("INSERT INTO `Accounts`('AccountID', 'Forname', 'Surname',
+                     'eMail', 'Username', 'Password')
+                     VALUES(?, ?, ?, ?, ?, ?)", details)
     conn.commit()
     conn.close()
     msg = "Completed."
     return msg
 
 
-@app.route("/AddDetails", methods = ['POST'])
+@app.route("/AddDetails", methods=['POST'])
 def AddDetails():
     firstname = request.form.get('firstname', default="Error")
     surname = request.form.get('surname', default="Error")
@@ -74,65 +74,85 @@ def AddDetails():
     circumstances = request.form.get('circumstances', default="Error")
 
     conn = sqlite3.connect(DATABASE)
-    details = [(firstname, surname, gender, dob, address1, address2, address3, address4, postcode, town, country, phone, fax, mobile, email, taxstatus, occupation, religion, circumstances)]
-    conn.executemany("INSERT INTO `Clients`('firstname', 'surname', 'gender', 'dob', 'address1', 'address2', 'address3', 'address4', 'postcode', 'town', 'country', 'phone', 'fax', 'mobile', 'email', 'taxstatus', 'occupation', 'religion', 'circumstances')\
-                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",details)
+    details = [(firstname, surname, gender, dob, address1, address2, address3,
+               address4, postcode, town, country, phone, fax, mobile, email,
+               taxstatus, occupation, religion, circumstances)]
+    conn.executemany("INSERT INTO `Clients`('firstname', 'surname', 'gender',
+                     'dob', 'address1', 'address2', 'address3', 'address4',
+                     'postcode', 'town', 'country', 'phone', 'fax', 'mobile',
+                     'email', 'taxstatus', 'occupation', 'religion',
+                     'circumstances')
+                     VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                            ?, ?)", details)
     conn.commit()
     conn.close()
     msg = "Details Added."
     return msg
 
+
 @app.route("/Client/ClientAdd")
 def ClientAdd():
-    return render_template('ClientData.html', msg = '')
+    return render_template('ClientData.html', msg='')
+
 
 @app.route("/AddClient")
 def customer():
-    return render_template('clientdetail.html', msg = '')
+    return render_template('clientdetail.html', msg='')
+
 
 @app.route("/Client")
 def clients():
-    return render_template('people/clients.html', msg = '')
+    return render_template('people/clients.html', msg='')
+
 
 @app.route("/AddDetails")
 def details():
-    return render_template('clientdetail.html', msg = '')
+    return render_template('clientdetail.html', msg='')
+
 
 @app.route("/taxStatus")
 def taxStatus():
-    return render_template('people/taxStatus.html', msg = '')
+    return render_template('people/taxStatus.html', msg='')
+
 
 @app.route("/Occupation")
 def occupation():
-    return render_template('people/occupation.html', msg = '')
+    return render_template('people/occupation.html', msg='')
+
 
 @app.route("/Dependants")
 def dependants():
-    return render_template('people/dependants.html', msg = '')
+    return render_template('people/dependants.html', msg='')
+
 
 @app.route("/Health")
 def health():
-    return render_template('people/health.html', msg = '')
+    return render_template('people/health.html', msg='')
+
 
 @app.route("/Expenditure")
 def expenditure():
-    return render_template('finances/expenditure.html', msg = '')
+    return render_template('finances/expenditure.html', msg='')
+
 
 @app.route("/Income")
 def income():
-    return render_template('finances/income.html', msg = '')
+    return render_template('finances/income.html', msg='')
+
 
 @app.route("/Liabilities")
 def liabilities():
-    return render_template('finances/liabilities.html', msg = '')
+    return render_template('finances/liabilities.html', msg='')
+
 
 @app.route("/Affordability")
 def affordability():
-    return render_template('finances/affordability.html', msg = '')
+    return render_template('finances/affordability.html', msg='')
+
 
 @app.route("/Assets")
 def assets():
-    return render_template('finances/assets.html', msg = '')
+    return render_template('finances/assets.html', msg='')
 
 if __name__ == "__main__":
     app.run(debug=True)
